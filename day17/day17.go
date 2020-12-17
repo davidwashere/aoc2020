5dafbbf2894134541421a63fe732e44a24cd09f8
notedo-action <20852399+davidwashere@users.noreply.github.com>
2020-12-17 01:58:37 -0600

    day17 p2 ugly

--- day17/day17.go
+++ day17/day17.go
@@ -64,10 +64,10 @@ func part1(inputfile string) int {
 	grids := map[int]*util.Grid{}
 	grids[0] = util.NewGridFromFile(inputfile, INACTIVE)
 
-	for z, grid := range grids {
-		fmt.Printf("[%v] %+v\n", z, grid)
-	}
-	fmt.Println()
+	// for z, grid := range grids {
+	// 	fmt.Printf("[%v] %+v\n", z, grid)
+	// }
+	// fmt.Println()
 
 	extents := 0
 	// each cycle adds a new -1
@@ -85,9 +85,9 @@ func part1(inputfile string) int {
 		g.SetExtents(o.GetMinX(), o.GetMinY(), o.GetMaxX(), o.GetMaxY())
 
 		// Expand the grids
-		for z, grid := range grids {
+		for _, grid := range grids {
 			grid.Grow(1)
-			fmt.Printf("[%v] %+v\n", z, grid)
+			// fmt.Printf("[%v] %+v\n", z, grid)
 		}
 
 		changes := []chg{}
@@ -117,8 +117,8 @@ func part1(inputfile string) int {
 	}
 
 	result := 0
-	for z, grid := range grids {
-		fmt.Printf("z %v, width %v, height %v\n", z, grid.Width(), grid.Height())
+	for _, grid := range grids {
+		// fmt.Printf("z %v, width %v, height %v\n", z, grid.Width(), grid.Height())
 		grid.VisitAll(func(x, y int, val string) {
 			if val == ACTIVE {
 				result++
@@ -162,6 +162,216 @@ func activeNeightbors(grids map[int]*util.Grid, origin C3D) int {
 	return count
 }
 
+// =============================
+type C4D struct {
+	x int
+	y int
+	z int
+	w int
+}
+
+var adj4d = []C4D{
+	// C3D{0, 0, 0}, // init
+	C4D{1, 0, 0, 0}, // w = 0
+	C4D{1, -1, 0, 0},
+	C4D{0, -1, 0, 0},
+	C4D{-1, -1, 0, 0},
+	C4D{-1, 0, 0, 0},
+	C4D{-1, 1, 0, 0},
+	C4D{0, 1, 0, 0},
+	C4D{1, 1, 0, 0},
+	C4D{0, 0, 1, 0}, // z = 1
+	C4D{1, 0, 1, 0},
+	C4D{1, -1, 1, 0},
+	C4D{0, -1, 1, 0},
+	C4D{-1, -1, 1, 0},
+	C4D{-1, 0, 1, 0},
+	C4D{-1, 1, 1, 0},
+	C4D{0, 1, 1, 0},
+	C4D{1, 1, 1, 0},
+	C4D{0, 0, -1, 0}, // Z = -1
+	C4D{1, 0, -1, 0},
+	C4D{1, -1, -1, 0},
+	C4D{0, -1, -1, 0},
+	C4D{-1, -1, -1, 0},
+	C4D{-1, 0, -1, 0},
+	C4D{-1, 1, -1, 0},
+	C4D{0, 1, -1, 0},
+	C4D{1, 1, -1, 0},
+	C4D{0, 0, 0, 1}, // w = 1
+	C4D{1, 0, 0, 1},
+	C4D{1, -1, 0, 1},
+	C4D{0, -1, 0, 1},
+	C4D{-1, -1, 0, 1},
+	C4D{-1, 0, 0, 1},
+	C4D{-1, 1, 0, 1},
+	C4D{0, 1, 0, 1},
+	C4D{1, 1, 0, 1},
+	C4D{0, 0, 1, 1}, // z = 1
+	C4D{1, 0, 1, 1},
+	C4D{1, -1, 1, 1},
+	C4D{0, -1, 1, 1},
+	C4D{-1, -1, 1, 1},
+	C4D{-1, 0, 1, 1},
+	C4D{-1, 1, 1, 1},
+	C4D{0, 1, 1, 1},
+	C4D{1, 1, 1, 1},
+	C4D{0, 0, -1, 1}, // Z = -1
+	C4D{1, 0, -1, 1},
+	C4D{1, -1, -1, 1},
+	C4D{0, -1, -1, 1},
+	C4D{-1, -1, -1, 1},
+	C4D{-1, 0, -1, 1},
+	C4D{-1, 1, -1, 1},
+	C4D{0, 1, -1, 1},
+	C4D{1, 1, -1, 1},
+	C4D{0, 0, 0, -1}, // w = -1
+	C4D{1, 0, 0, -1},
+	C4D{1, -1, 0, -1},
+	C4D{0, -1, 0, -1},
+	C4D{-1, -1, 0, -1},
+	C4D{-1, 0, 0, -1},
+	C4D{-1, 1, 0, -1},
+	C4D{0, 1, 0, -1},
+	C4D{1, 1, 0, -1},
+	C4D{0, 0, 1, -1}, // z = 1
+	C4D{1, 0, 1, -1},
+	C4D{1, -1, 1, -1},
+	C4D{0, -1, 1, -1},
+	C4D{-1, -1, 1, -1},
+	C4D{-1, 0, 1, -1},
+	C4D{-1, 1, 1, -1},
+	C4D{0, 1, 1, -1},
+	C4D{1, 1, 1, -1},
+	C4D{0, 0, -1, -1}, // Z = -1
+	C4D{1, 0, -1, -1},
+	C4D{1, -1, -1, -1},
+	C4D{0, -1, -1, -1},
+	C4D{-1, -1, -1, -1},
+	C4D{-1, 0, -1, -1},
+	C4D{-1, 1, -1, -1},
+	C4D{0, 1, -1, -1},
+	C4D{1, 1, -1, -1},
+}
+
+type chg4d struct {
+	c   C4D
+	val string
+}
+
 func part2(inputfile string) int {
-	return 0
+	grids := map[int]map[int]*util.Grid{}
+	grids[0] = map[int]*util.Grid{}
+	grids[0][0] = util.NewGridFromFile(inputfile, INACTIVE)
+
+	for z, grid := range grids {
+		fmt.Printf("[%v] %+v\n", z, grid)
+	}
+	fmt.Println()
+
+	o := grids[0][0]
+	extents := 0
+	// each cycle adds a new -1
+	for cycle := 0; cycle < 6; cycle++ {
+		extents++
+		for z := -extents; z <= extents; z++ {
+			if _, ok := grids[z]; ok {
+				grids[z][extents] = util.NewGrid(INACTIVE)
+				grids[z][extents].SetExtents(o.GetMinX(), o.GetMinY(), o.GetMaxX(), o.GetMaxY())
+				grids[z][-extents] = util.NewGrid(INACTIVE)
+				grids[z][-extents].SetExtents(o.GetMinX(), o.GetMinY(), o.GetMaxX(), o.GetMaxY())
+			} else { // doesn't exist yet
+				grids[z] = map[int]*util.Grid{}
+				for w := -extents; w <= extents; w++ {
+					grids[z][w] = util.NewGrid(INACTIVE)
+					grids[z][w].SetExtents(o.GetMinX(), o.GetMinY(), o.GetMaxX(), o.GetMaxY())
+				}
+			}
+		}
+
+		for w := -extents; w <= extents; w++ {
+			for z := -extents; z <= extents; z++ {
+				grids[z][w].Grow(1)
+			}
+		}
+
+		changes := []chg4d{}
+		for w := -extents; w <= extents; w++ {
+			for z := -extents; z <= extents; z++ {
+				grid := grids[z][w]
+				grid.VisitAll(func(x, y int, val string) {
+					c := C4D{x, y, z, w}
+					nActive := activeNeightbors4(grids, c)
+
+					if val == ACTIVE && (nActive < 2 || nActive > 3) {
+						changes = append(changes, chg4d{
+							c:   c,
+							val: INACTIVE,
+						})
+					} else if val == INACTIVE && nActive == 3 {
+						changes = append(changes, chg4d{
+							c:   c,
+							val: ACTIVE,
+						})
+					}
+				})
+			}
+		}
+
+		for _, change := range changes {
+			set4(grids, change)
+		}
+	}
+
+	result := 0
+	for w := -extents; w <= extents; w++ {
+		for z := -extents; z <= extents; z++ {
+			grid := grids[z][w]
+			grid.VisitAll(func(x, y int, val string) {
+				if val == ACTIVE {
+					result++
+				}
+			})
+		}
+	}
+
+	return result
+}
+
+func get4(grids map[int]map[int]*util.Grid, c C4D) string {
+	if _, ok := grids[c.z]; !ok {
+		return INACTIVE
+	}
+
+	if _, ok := grids[c.z][c.w]; !ok {
+		return INACTIVE
+	}
+
+	grid := grids[c.z][c.w]
+
+	return grid.Get(c.x, c.y)
+}
+
+func set4(grids map[int]map[int]*util.Grid, c chg4d) {
+	grid := grids[c.c.z][c.c.w]
+
+	grid.Set(c.c.x, c.c.y, c.val)
+}
+
+func activeNeightbors4(grids map[int]map[int]*util.Grid, origin C4D) int {
+	count := 0
+	for _, v := range adj4d {
+		c := C4D{
+			x: origin.x + v.x,
+			y: origin.y + v.y,
+			z: origin.z + v.z,
+			w: origin.w + v.w,
+		}
+
+		if get4(grids, c) == ACTIVE {
+			count++
+		}
+	}
+
+	return count
 }
--- day17/day17_test.go
+++ day17/day17_test.go
@@ -44,28 +44,28 @@ func TestPointers(t *testing.T) {
 func TestP1(t *testing.T) {
 	got := part1("sample.txt")
 	fmt.Printf("Got: %v\n", got)
-	// want := 0
-	// if got != want {
-	// 	t.Errorf("Got %v but want %v", got, want)
-	// }
+	want := 112
+	if got != want {
+		t.Errorf("Got %v but want %v", got, want)
+	}
 }
 
 func TestP1_Actual(t *testing.T) {
 	got := part1("input.txt")
 	fmt.Printf("Got: %v\n", got)
-	// want := 0
-	// if got != want {
-	// 	t.Errorf("Got %v but want %v", got, want)
-	// }
+	want := 301
+	if got != want {
+		t.Errorf("Got %v but want %v", got, want)
+	}
 }
 
 func TestP2(t *testing.T) {
 	got := part2("sample.txt")
 	fmt.Printf("Got: %v\n", got)
-	// want := 0
-	// if got != want {
-	// 	t.Errorf("Got %v but want %v", got, want)
-	// }
+	want := 848
+	if got != want {
+		t.Errorf("Got %v but want %v", got, want)
+	}
 }
 
 func TestP2_Actual(t *testing.T) {
